b2064df55e38a40cd00a3af011f4a3708e5a8aff
Yura <rkdldk37@gmail.com>
2024-08-08 16:57:59 +0900

    20240808commit

--- board/src/main/java/org/scoula/board/mapper/BoardMapper.java
+++ board/src/main/java/org/scoula/board/mapper/BoardMapper.java
@@ -7,7 +7,19 @@ import java.util.List;
 
 public interface BoardMapper {
     // tbl_board 테이블에서 모든 컬럼을 no 컬럼 기준으로 내림차순 정렬
-    @Select("select * from tbl_board order by no desc")
+//    @Select("select * from tbl_board order by no desc")
     // BoardVO 객체의 리스트를 반환
     public List<BoardVO> getList();
+
+//    특정 번호(no)에 해당하는 BoardVO 객체 반환
+    public BoardVO get(Long no);
+
+//    새로운 BoradVO 게시물을 생성
+    public void create(BoardVO board);
+
+//    기존에 있는 게시글을 업데이트(수정)
+    public int update(BoardVO board);
+
+//    해당 no 값을 가진 게시글을 삭제
+    public int delete(Long no);
 }
